0ac84704697d81dcaae1655a6afbea2c70d240f0
Sagar Dhadke <peaceunique2003@gmail.com>
2024-03-04 23:40:44 +0530

    Initial Commit

---
+++ app/src/main/java/net/uniquecomputer/myweb/MainActivity.kt
@@ -0,0 +1,63 @@
+package net.uniquecomputer.myweb
+
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import net.uniquecomputer.myweb.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.WebView.loadUrl("https://skinfotechies.com/")
+
+        binding.WebView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            allowFileAccess = true
+        }
+
+        binding.WebView.webViewClient = object : WebViewClient(){
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                binding.ProgressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                binding.ProgressBar.visibility = View.GONE
+            }
+
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (binding.WebView.canGoBack()){
+            binding.WebView.goBack()
+        }else{
+            super.onBackPressed()
+        }
+    }
+
+}
